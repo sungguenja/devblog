cf2a8c8c23b9bef39be3d7e97e75f7ab39d7fb02
sungguenja <sungguenja@gmail.com>
2021-11-01 23:30:24 +0900

    feat-back: 메뉴와 게시글 목록 가져오기 생성

--- backend/writing/urls.py
+++ backend/writing/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 app_name = 'writing'
 urlpatterns = [
+    path('menus/',views.getMenu,name='menus'),
+    path('menus/<int:menu_pk>/',views.getArticlesWithMenu,name='getArticlesWithMenu'),
+    path('article/<int:article_pk>/',views.getArticleDetail,name='getArticleDetail'),
     
 ]
--- backend/writing/views.py
+++ backend/writing/views.py
@@ -1,3 +1,20 @@
+from django.http.response import JsonResponse
 from django.shortcuts import render
+from writing.models import Article, ArticleHashTag, HashTag, Menu
 
 # Create your views here.
+def getMenu(request):
+    menu_list = Menu.objects.all()
+    return JsonResponse({'menu_list':list(menu_list)})
+
+def getArticlesWithMenu(request,menu_pk):
+    article_list = Article.objects.filter(menu_pk=menu_pk)
+    return JsonResponse({'article_list':list(article_list)})
+
+def getArticleDetail(request,article_pk):
+    article = Article.objects.get(id=article_pk)
+    hash_tag_nn_list = ArticleHashTag.objects.filter(article_pk=article_pk)
+    hash_tag_list = []
+    for hash_tag in hash_tag_nn_list:
+        hash_tag_list.append(HashTag.objects.get(id=hash_tag.hashtag_pk))
+    return JsonResponse({'article':article,'hash_tag_list':list(hash_tag_list)})
