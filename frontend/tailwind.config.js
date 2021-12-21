c3de25d3c46ea19630f3e8d2a66acf2b9880185f
sungguenja <sungguenja@gmail.com>
2021-12-21 23:27:11 +0900

    feat-frontend: tailwindcss 추가

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,ts,jsx,tsx}",
+    "./Components/**/*.{html,js,ts,jsx,tsx}",
+    "./pages/**/*.{html,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
