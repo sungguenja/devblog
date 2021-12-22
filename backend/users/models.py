503036204d5f6a23a3bc5d5ff48d6940f3118ce5
sungguenja <sungguenja@gmail.com>
2021-12-22 22:24:04 +0900

    feat-backend: 유저 모델 default값 수정 및 모델들 admin등록

--- backend/users/models.py
+++ backend/users/models.py
@@ -3,10 +3,10 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class User(AbstractUser):
-    nickname = models.CharField(max_length=255)
-    github_id = models.IntegerField()
-    node_id = models.TextField()
-    github_url = models.TextField()
+    nickname = models.CharField(max_length=255,default='')
+    github_id = models.IntegerField(default=0)
+    node_id = models.TextField(default='')
+    github_url = models.TextField(default='')
 
     def __str__(self):
         return self.nickname + " " + self.github_url
--- backend/writing/admin.py
+++ backend/writing/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import Category,Menu,Article,HashTag,ArticleHashTag,Comment,Bookmark,Like
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Menu)
+admin.site.register(Article)
+admin.site.register(HashTag)
+admin.site.register(ArticleHashTag)
+admin.site.register(Comment)
+admin.site.register(Bookmark)
+admin.site.register(Like)
