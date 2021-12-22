0785162b0e3add4e6dda280a7257a09088240a90
sungguenja <sungguenja@gmail.com>
2021-12-22 23:47:51 +0900

    fix-backend: serialize를 넣어 리턴값 에러 없앰

--- backend/writing/views.py
+++ backend/writing/views.py
@@ -1,5 +1,6 @@
-from django.http.response import JsonResponse
-from django.shortcuts import render
+from django.http.response import HttpResponse, JsonResponse
+from django.core import serializers
+import json
 from writing.models import Article, ArticleHashTag, HashTag, Menu
 
 # Create your views here.
@@ -9,7 +10,8 @@ def getMenu(request):
 
 def getArticlesWithMenu(request,menu_pk):
     article_list = Article.objects.filter(menu_pk=menu_pk)
-    return JsonResponse({'article_list':list(article_list)})
+    article_serialized = serializers.serialize('json',article_list)
+    return HttpResponse(article_serialized,content_type="text/json-comment-filtered")
 
 def getArticleDetail(request,article_pk):
     article = Article.objects.get(id=article_pk)
