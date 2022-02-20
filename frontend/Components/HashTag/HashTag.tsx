d587873e1c32fd6efb6905cb113387183bf3228f
sungguenja <sungguenja@gmail.com>
2022-02-21 01:04:45 +0900

    feat-frontend: 스타일 적용

--- frontend/Components/HashTag/HashTag.tsx
+++ frontend/Components/HashTag/HashTag.tsx
@@ -5,7 +5,11 @@ interface hashTagProps {
 }
 
 const hashTag = ({ hashTagObj }: hashTagProps) => {
-  return <h1>#{hashTagObj.fields.title}</h1>;
+  return (
+    <span className="bg-green-100 text-green-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-green-200 dark:text-green-900">
+      #{hashTagObj.fields.title}
+    </span>
+  );
 };
 
 export default hashTag;
