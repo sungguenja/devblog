072ecb43890ee03f2133d15b5cf673222d3051d1
sungguenja <sungguenja@gmail.com>
2022-02-21 01:04:28 +0900

    feat-frontend: image태그 사용

--- frontend/Components/ArticleMain/ArticleMain.tsx
+++ frontend/Components/ArticleMain/ArticleMain.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import remarkMath from "remark-math";
@@ -21,6 +22,26 @@ const articleMain = ({ nowArticle }: ArticleMainProps) => {
       rehypePlugins={[rehypeKatex]}
       children={nowArticle.fields.content}
       components={{
+        p: ({ node, children }) => {
+          if (node.children[0].tagName === "img") {
+            const image: any = node.children[0];
+            console.log(image);
+            return (
+              <div className="flex justify-center">
+                <div className="">
+                  <Image
+                    src={image.properties.src}
+                    alt={image.properties.alt}
+                    width="600"
+                    height="300"
+                  />
+                  <p className="text-center">{image.properties.alt}</p>
+                </div>
+              </div>
+            );
+          }
+          return <p>{children}</p>;
+        },
         code({ node, inline, className, children, ...props }) {
           const match = /language-(\w+)/.exec(className || "");
           return !inline && match ? (
