a9bd7b63afd025e23623a77548740abb816ec4d3
sungguenja <sungguenja@gmail.com>
2022-02-21 01:03:32 +0900

    feat-frontend: 댓글 입력창 스타일 적용

--- frontend/Components/CommentForm/CommentForm.tsx
+++ frontend/Components/CommentForm/CommentForm.tsx
@@ -1,6 +1,6 @@
 import { ChangeEvent, SyntheticEvent } from "react";
 
-import { Comment } from "Interfaces/writing";
+import style from "./CommentForm.module.css";
 
 interface CommentFormProps {
   postCommentWithValue: (event: SyntheticEvent) => void;
@@ -21,29 +21,105 @@ const CommentForm = ({
 }: CommentFormProps) => {
   return (
     <form onSubmit={postCommentWithValue}>
-      <input
-        type="text"
-        name="nickname"
-        id="nickname"
-        value={defaultNickname}
-        onChange={handleChangeNickname}
-        className={isLogin ? "hidden" : ""}
-      />
-      <input
-        type="password"
-        name="password"
-        id="password"
-        className={isLogin ? "hidden" : ""}
-      />
-      <br className={isLogin ? "hidden" : ""} />
-      <textarea
-        cols={30}
-        name="comment"
-        id="comment"
-        value={defaultContent}
-        onChange={handleChangeContent}
-      />
-      <button type="submit">submit</button>
+      <div className="grid grid-cols-4 gap-4">
+        <div className="form-group mb-6">
+          <input
+            type="text"
+            className={`form-control 
+            block 
+            w-full 
+            px-3 
+            py-1.5 
+            text-base 
+            font-normal 
+            text-gray-700 
+            bg-white bg-clip-padding 
+            border border-solid border-gray-300 
+            rounded 
+            transition 
+            ease-in-out 
+            m-0 
+            focus:text-gray-700 focus:bg-white focus:border-green-600 focus:outline-none ${
+              isLogin ? "hidden" : ""
+            }`}
+            name="nickname"
+            id="nickname"
+            value={defaultNickname}
+            onChange={handleChangeNickname}
+            aria-describedby="nickname"
+            placeholder="닉네임"
+          />
+        </div>
+        <div className="form-group mb-6">
+          <input
+            className={`form-control
+          block
+          w-full
+          px-3
+          py-1.5
+          text-base
+          font-normal
+          text-gray-700
+          bg-white bg-clip-padding
+          border border-solid border-gray-300
+          rounded
+          transition
+          ease-in-out
+          m-0
+          focus:text-gray-700 focus:bg-white focus:border-green-600 focus:outline-none ${
+            isLogin ? "hidden" : ""
+          } `}
+            type="password"
+            name="password"
+            id="password"
+            aria-describedby="password"
+            placeholder="비밀번호"
+          />
+        </div>
+      </div>
+      <div className="form-group mb-6">
+        <textarea
+          className="form-control block
+        w-full
+        px-3
+        py-1.5
+        text-base
+        font-normal
+        text-gray-700
+        bg-white bg-clip-padding
+        border border-solid border-gray-300
+        rounded
+        transition
+        ease-in-out
+        m-0
+        focus:text-gray-700 focus:bg-white focus:border-green-600 focus:outline-none"
+          cols={20}
+          name="comment"
+          id="comment"
+          value={defaultContent}
+          onChange={handleChangeContent}
+          placeholder="댓글을 입력해주세요"
+        />
+      </div>
+      <button
+        type="submit"
+        className={`
+      w-full
+      px-6
+      py-2.5
+      text-white
+      font-medium
+      text-xs
+      leading-tight
+      uppercase
+      rounded
+      shadow-mdactive:shadow-lg
+      transition
+      duration-150
+      ease-in-out ${style.submitButton}`}
+      >
+        댓글 작성
+      </button>
     </form>
   );
 };
