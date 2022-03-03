d8a99cc3d1558ebbb688fc74002644e986c7dbec
sungguenja <sungguenja@gmail.com>
2022-03-04 02:50:15 +0900

    feat-backend: url 라우팅을 위한 추가

--- backend/mydevblog/urls.py
+++ backend/mydevblog/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('users/', include('users.urls',namespace='users')),
-    path('writing/',include('writing.urls',namespace='writing')),
+    path('backend/admin/', admin.site.urls),
+    path('backend/users/', include('users.urls',namespace='users')),
+    path('backend/writing/',include('writing.urls',namespace='writing')),
 ]
