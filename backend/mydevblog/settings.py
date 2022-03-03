3ab9e7b5bd36f9e58c869752e684528655ee44d0
sungguenja <sungguenja@gmail.com>
2022-03-04 02:15:03 +0900

    feat-backend: cors추가

--- backend/mydevblog/settings.py
+++ backend/mydevblog/settings.py
@@ -85,12 +85,16 @@ CORS_ORIGIN_WHITELIST = (
     'http://localhost:3000',
     'http://127.0.0.1:3000',
     'http://0.0.0.0:3000',
+    'http://mydevblog.xyz/',
+    'https://mydevblog.xyz/',
 )
 
 CORS_TRUSTED_ORIGINS = (
     'http://localhost:3000',
     'http://127.0.0.1:3000',
     'http://0.0.0.0:3000',
+    'http://mydevblog.xyz/',
+    'https://mydevblog.xyz/',
 )
 
 # cors headers
