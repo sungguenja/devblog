48514249ded1d2a48c8cc71139c2a3a375668f13
sungguenja <sungguenja@gmail.com>
2022-04-13 22:45:42 +0900

    feat-backend: article모델에 description 속성 추가

--- backend/writing/models.py
+++ backend/writing/models.py
@@ -22,6 +22,7 @@ class Article(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     github_url = models.TextField(default='No')
+    description = models.CharField(max_length=255,default="description",null=True,blank=True)
 
     def __str__(self):
         return self.title
